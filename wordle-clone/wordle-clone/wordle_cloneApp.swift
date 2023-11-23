15605607dbf894ae70e1d726e19eda42a50a34c3
BenjaminKaheel <benjaminkaheel1@gmail.com>
2023-11-23 06:58:23 -0500

    Built Launch Screen and configured interface orientations

---
+++ wordle-clone/wordle-clone/wordle_cloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  wordle_cloneApp.swift
+//  wordle-clone
+//
+//  Created by Benjamin Kaheel on 11/23/23.
+//
+
+import SwiftUI
+
+@main
+struct wordle_cloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
